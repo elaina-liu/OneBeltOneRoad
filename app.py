6c91096779364e89d99a59406cc7f76028cf440c
elaina-liu <61395318+elaina-liu@users.noreply.github.com>
2022-10-28 12:26:01 -0400

    complete homepage

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="/static")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
