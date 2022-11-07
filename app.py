56d804c1d80f7647cbb6d4b4a9003ab60a827cc5
wenhuihuang <wenhuihu@buffalo.edu>
2022-11-06 23:36:02 -0500

    finish front-end and back-end connection

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
+import json
 
 app = Flask(__name__, static_url_path="/static")
-
+app.config['SECRET'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if "User connected!" not in message:
+        term_info = json.loads(message)
+        print(term_info["user"])
 
 
 if __name__ == '__main__':
     app.run()
+
--- static/javascript.js
+++ static/javascript.js
@@ -1,28 +1,48 @@
-let steps;
-
-
-// ---------- DICE START HERE ----------
-let images = ["static/img/dice_1.png", "static/img/dice_2.png", "static/img/dice_3.png", "static/img/dice_4.png", "static/img/dice_5.png", "static/img/dice_6.png"];
-let dice = document.querySelectorAll("img[class=die]");
-
-function roll(){
-  dice.forEach(function(die){
-    die.classList.add("shake");
-  });
-  setTimeout(function(){
-    dice.forEach(function(die){
-      die.classList.remove("shake");
+$(document).ready(function() {
+    var socket = io.connect("http://localhost:5000");
+    let socket_id = Math.floor(Math.random() * 100);
+    let steps = 0;
+    socket.on('connect', function() {
+      // console.log(socket_id)
+      socket.send(socket_id.toString() + ": User connected!");
     });
-    let dieOneValue = Math.floor(Math.random() * 6);
-    // let dieTwoValue = Math.floor(Math.random() * 6);
 
-    steps = dieOneValue + 1;
-    console.log(steps)
+    // ---------- DICE START HERE ----------
+    $(".dice-button").click(function (){
+        steps = roll();
+        setTimeout(function(){
+            console.log("I am here");
+            console.log("this roll: " + steps);
+            socket.send(JSON.stringify({'user':  socket_id.toString(), 'step': steps.toString()}));
+            // socket.send("User" + socket_id.toString() + ": " + steps.toString());
+        }, 1500);
+    });
 
-    // console.log(dieOneValue+1, dieTwoValue+1);
-    document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
-    // document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-  },
-  1000
-  );
-}
+    let images = ["static/img/dice_1.png", "static/img/dice_2.png", "static/img/dice_3.png", "static/img/dice_4.png", "static/img/dice_5.png", "static/img/dice_6.png"];
+    let dice = document.querySelectorAll("img[class=die]");
+
+    function roll(){
+      dice.forEach(function(die){
+        die.classList.add("shake");
+      });
+      setTimeout(function(){
+        dice.forEach(function(die){
+          die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random() * 6);
+        // let dieTwoValue = Math.floor(Math.random() * 6);
+
+        steps = dieOneValue + 1;
+        // console.log(dieOneValue+1, dieTwoValue+1);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        // document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+      },
+      1000
+      );
+    }
+})
+
+
+
+
+
